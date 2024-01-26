224017ece30cddc8b6b3027cc3cb8191a4d94c78
Aiman <65372518+Aiman7756@users.noreply.github.com>
2024-01-26 01:22:58 -0800

    start on media page
    delete old media page
    start on content boxes for media
    edit gamebox bg

---
+++ entry_data.js
@@ -0,0 +1,58 @@
+let entry_data = {
+  "archanea": {},
+  "sov": {},
+  "gothw": [
+  {
+      "name": "Genealogy of the Holy War (OOSAWA Mitsuki)",
+      "story" : "Mitsuki Oosawa",
+      "art" : "Mitsuki Oosawa",
+      "type" : "manga",
+      "translation" : [
+        "Another FE4 Translation",
+        "FalaScans",
+        "HolsetyScans"],
+      "img": "gothw_oosawa",
+      "raws" : "",
+      "buttons": [{
+          "type": "google",
+          "link" : "https://drive.google.com/drive/folders/1Ah26-A1hxYMXG2RbpOIRuHbhtW-WorVs",
+          "note": "Vol 13-16"
+        },
+        {
+          "type": "mangadex",
+          "link" : "https://mangadex.org/title/81e9cf75-908b-489e-906d-c7f001673274/fire-emblem-genealogy-of-the-holy-war",
+          "note": "Vol 1-16"
+        }
+      ]
+    },
+    {
+      "name": "Genealogy of the Holy War (FUJIMORI Nattsu)",
+      "story" : "Fujimori Nattsu",
+      "art" : "Fujimori Nattsu",
+      "type" : "manga",
+      "translation" : [
+        "TheEnd",
+        "FranceFE"],
+      "img": "gothw_oosawa",
+      "raws" : "",
+      "buttons": [
+        {
+          "type": "mangadex",
+          "link" : "https://mangadex.org/title/9259bdc2-d6f1-4bf8-b4a7-725f34f4b8fd/fire-emblem-genealogy-of-the-holy-war",
+          "note": "Ch 1-38"
+        }
+      ]
+    }
+  ],
+
+  "t776": {},
+  "binding": {},
+  "blazing": {},
+  "ss": {},
+  "por": {},
+  "rd": {},
+  "awakening": {},
+  "fates": {},
+  "3h": {},
+  "engage": {}
+}
--- scripts.js
+++ scripts.js
@@ -44,55 +44,25 @@ function setUp() {
   }
 }
 
-function toggleCollapsible(id, contentID) {
-  let el = document.getElementById(id);
-  el.classList.toggle("active");
-  var content = document.getElementById(contentID)
-  if (content.style.maxHeight) {
-    content.style.maxHeight = null;
-  } else {
-    content.style.maxHeight = content.scrollHeight + "px";
-  }
-}
+function mediaPage(game) {
+  let items = entry_data[game]
+  media_div = document.getElementById('media_div')
+  for (let i = 0; i < items.length; i++){
+    let div = document.createElement("div");
+    div.classList.toggle('media_box')
 
-function generateChTable(tableID, chs) {
-  let tablestr = document.getElementById(tableID).innerHTML;
-  tablestr = ` <table class="chTable"> <tbody>`
-  for (let i = 0; i < chs.length; i += 2) {
-    tablestr += '<tr>'
-    for (let j = 0; j < 2 && (i + j < chs.length); j++) {
-      tablestr += `<td> <a target='_blank' href='${chs[i+j].link}'>${chs[i+j].title}</a></td>`
-    }
-    tablestr += '</tr>'
-  }
-  tablestr += '</tbody></table>'
-  document.getElementById(tableID).innerHTML = tablestr
-}
+    let img = document.createElement("img")
+    img.classList.toggle('media_img')
+    img.src = `./covers/${items[i]['img']}.png`
 
-function display(cboxID, clss) {
-  closeAll();
-  closeAll();
-  closeAll();
-  nodes = document.getElementsByClassName(clss)
-  if (document.getElementById(cboxID).checked) {
-    for (let i = 0; i < nodes.length; i++) {
-      nodes[i].style.display = 'table';
-    }
-  } else {
-    for (let i = 0; i < nodes.length; i++) {
-      nodes[i].style.display = 'none';
-    }
-  }
-}
 
-function closeAll() {
-  let nodes = document.getElementsByClassName('active')
-  console.log(nodes)
-  for (let i = 0; i < nodes.length; i++) {
-    if (nodes[i].id == 'filters1Coll' || nodes[i].id == 'filters2Coll') {
-      continue;
-    }
-    console.log(nodes[i].id)
-    this.toggleCollapsible(nodes[i].id, map[nodes[i].id])
+    let span = document.createElement("span")
+    span.innerHTML = `${items[i]['name']}`
+    div.appendChild(span)
+    div.appendChild(img)
+
+    media_div.appendChild(div)
+
+
   }
 }
